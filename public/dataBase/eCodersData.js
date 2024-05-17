0f9dce933e32c4dfafd7ca0794760eecb0f288e4
DEVBITAN <jquirama96@gmail.com>
2024-05-17 17:08:55 -0500

    folder creation

---
+++ public/dataBase/eCodersData.js
@@ -0,0 +1,14 @@
+let dataBaseEcoders = [
+    {
+        name: "Toribio",
+        lastName: "Santa",
+        email: "torisanta@gmail.com",
+        country: "peru",
+        phone: 3976893091,
+        core: "back",
+        skills: ["php, nodejs, sql, java, javascript"],
+        ageExperience: 3,
+        idioms: ["English", "french", "Spanish"],
+        userImage: "https://picsum.photos/200"
+    }
+]
