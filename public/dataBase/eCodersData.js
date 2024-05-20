4249cb5ec06aaf7865f8dc0bcf20cf46726463ad
DEVBITAN <jquirama96@gmail.com>
2024-05-19 19:38:55 -0500

    feat:function register

--- public/dataBase/eCodersData.js
+++ public/dataBase/eCodersData.js
@@ -1,7 +1,46 @@
-export let dataBaseEcoders = [
+export const dataBaseEcoders = [
     {
         id: 1,
-        name: "Toribio",
+        isName: "Toribio",
+        lastName: "Santa",
+        email: "torisanta@gmail.com",
+        country: "peru",
+        phone: 3976893091,
+        core: "back",
+        skills: ["php, nodejs, sql, java, javascript"],
+        ageExperience: 3,
+        idioms: ["English", "french", "Spanish"],
+        // userImage: "https://picsum.photos/200"
+    },
+    {
+        id: 2,
+        isName: "Toribio2",
+        lastName: "Santa",
+        email: "torisanta@gmail.com",
+        country: "peru",
+        phone: 3976893091,
+        core: "back",
+        skills: ["php, nodejs, sql, java, javascript"],
+        ageExperience: 3,
+        idioms: ["English", "french", "Spanish"],
+        // userImage: "https://picsum.photos/200"
+    },
+    {
+        id: 3,
+        isName: "Toribio3",
+        lastName: "Santa",
+        email: "torisanta@gmail.com",
+        country: "peru",
+        phone: 3976893091,
+        core: "back",
+        skills: ["php, nodejs, sql, java, javascript"],
+        ageExperience: 3,
+        idioms: ["English", "french", "Spanish"],
+        // userImage: "https://picsum.photos/200"
+    },
+    {
+        id: 4,
+        isName: "Toribio4",
         lastName: "Santa",
         email: "torisanta@gmail.com",
         country: "peru",
--- src/js/authRegisterCoder.js
+++ src/js/authRegisterCoder.js
@@ -1,11 +1,12 @@
-import Swal from 'sweetalert2';
 import '../styles/styles.scss' // Import our custom CSS
 import * as bootstrap from 'bootstrap' // Import all of Bootstrap's JS
-import { dataBaseEcoders } from '../../public/dataBase/eCodersData';
-// import { genericMessageAlert } from './alerts';
+import { genericMessageAlert } from './alerts';
+import { dataBaseEcoders as defaultDataBaseEcoders } from '../../public/dataBase/eCodersData';
+
+let dataBaseEcoders = JSON.parse(localStorage.getItem('users')) || defaultDataBaseEcoders // compara la inicial con la agregada a ver cual tiene mas info
 
 const form = document.getElementById('form');
-const name = document.getElementById('name');
+const isName = document.getElementById('name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const country = document.getElementById('country')
@@ -15,50 +16,36 @@ const skills = document.getElementById('skills')
 const ageExperience = document.getElementById('age')
 const idioms = document.getElementById('idioms')
 // const userImage = document.getElementById('user-image')
+console.log(dataBaseEcoders);
 
-
-
-
-const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
+function register(event, isName, lastName, email, country, phone, core, skills, ageExperience, idioms, dataBaseEcoders) {
+    event.preventDefault();
+    if (form.checkValidity()) {
+        const newUser = {
+            id: Date.now(),
+            isName: isName.value,
+            lastName: lastName.value,
+            email: email.value,
+            country: country.value,
+            phone: phone.value,
+            core: core.value,
+            skills: skills.value,
+            ageExperience: ageExperience.value,
+            idioms: idioms.value
+        }
+        dataBaseEcoders.push(newUser)
+        localStorage.setItem('users', JSON.stringify(dataBaseEcoders)) // agregar base de datos a la memoria del navegador (tener los datos 'globales)
+        genericMessageAlert('user saved')
+        form.reset();
+        form.classList.add("is-invalid");
+    } else {
+        console.log("mostrar campos que no estan válidos");
+        form.classList.add("was-validated");
     }
-});
-
-export function genericMessageAlert(msg) {
-    Toast.fire({
-        icon: "success",
-        title: msg
-    });
 }
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    console.log(event);
-    console.log('hi');
-    genericMessageAlert('xd')
 
+form.addEventListener('submit', function (event) {
+    register(event, isName, lastName, email, country, phone, core, skills, ageExperience, idioms, dataBaseEcoders)
 })
-
-function register(event, name, lastName, email, country, phone, core, skills, ageExperience, idioms) {
-    const newUser = {
-        id: Date.now(),
-        name,
-        lastName,
-        email,
-        country,
-        phone,
-        core,
-        skills,
-        ageExperience,
-        idioms,
-    }
-    dataBaseEcoders.push(newUser)
-}
 
