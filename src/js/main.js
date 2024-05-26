4725f647fe65b466da7db3af069b4f2180b28586
dayana benjumea <lbenjumb07@gmail.com>
2024-05-26 14:48:27 -0500

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,32 @@ import * as bootstrap from 'bootstrap' // Import all of Bootstrap's JS
 
 // }
 
+// Obtener elementos del DOM
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+var form = document.getElementById("requestForm");
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+    alert("Solicitud enviada con exito");
+    modal.style.display = "none";
+}
