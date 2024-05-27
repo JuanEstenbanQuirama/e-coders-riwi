ea0291502569f4d2c99b88777df0a3cfd8f364ea
Dacardonac <dacardonac201@gmail.com>
2024-05-26 19:09:14 -0500

    fix: filter

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,15 +1,12 @@
-
-
 // (function () { // guardian 
 //     const userOnline = localStorage.getItem('userOnline')
-    
+
 //     if(userOnline === null) {
 //         window.location.href="/"
 //     } else {
 //         console.log(userOnline);
 //     }
 // })()
-
 
 const btnLogout = document.getElementById('btn-logout');
 
@@ -26,43 +23,27 @@ function getData() {
 
 getData();
 
-// Escucha del botón de búsqueda
-// document.getElementById('search').addEventListener('click', async () => {
-//     const getSkills = document.getElementById('search-input').value;
-//     await createCards(getSkills);
-// });
-
-// // Escucha de la tecla 'Enter' en el campo de entrada
-// document.getElementById('search-input').addEventListener('keyup', async (event) => {
-//     if (event.key === 'Enter') {
-//         const getSkills = event.target.value;
-//         await createCards(getSkills);
-//     }
-// });
-
-// async function getSkills(skills) {
-//     const URLCODERS = "http://localhost:3000/authCoders";
-//     const response = await fetch(`${URLCODERS}${skills === 'All' ? '' : `?coderSkills=${skills}`}`);
-//     return await response.json();
-// };
-
-async function getCoders(filterText) {
+async function getCoders(options) {
+    const skills = options.coderSkills;
+    const coderCore = options.coderCore;
     const URLCODERS = "http://localhost:3000/authCoders";
+    const coderCoreParam = (!coderCore || coderCore === 'All') ? '' : `?coderCore=${coderCore}`; // Cuando tiene ! al principio significa un NO y evalua si esta vacio, null o undefined
+    const coderSkillsParam = !skills ? '' : `?coderSkills_like=${skills}`
     // Forma la URl que me hace el llamado a la API que trae la info de la base de datos
-    const response = await fetch(`${URLCODERS}${filterText === 'All' ? '' : `?coderCore=${filterText}`}`);
+    const response = await fetch(`${URLCODERS}${coderCoreParam}${coderSkillsParam}`);
     return await response.json();
 };
+
 // Funcion de filtrado
-async function createCards(filterText) {
+async function createCards(options) {
     document.getElementById("products").replaceChildren(); // Limpia las cards
-    const coders = await getCoders(filterText);
+    const coders = await getCoders(options);
     // Proceso de creación de las cards
     for (let coder of coders) {
 
         // Card
         let card = document.createElement("div");
         card.classList.add("card");
-        card.innerHTML = ``
         card.innerHTML = `
             <article class="container">
                 <figure class="card-img">
@@ -70,7 +51,7 @@ async function createCards(filterText) {
                 </figure>
                 <h5 class="name card-title">${coder.coderName.charAt(0).toUpperCase() + coder.coderName.slice(1).toLowerCase() + " " + coder.coderLastName.charAt(0).toUpperCase() + coder.coderLastName.slice(1).toLowerCase()}</h5>
                 <h6 class="core card-subtitle">${coder.coderCore.charAt(0).toUpperCase() + coder.coderCore.slice(1).toLowerCase()}</h6>
-                <p class="skills">${coder.coderSkills}</p>
+                <p class="skills">${coder.coderSkills.join(" - ")}</p>
                 <hr class="card-divider">
                 <div class="card-footer">
                     <button class="card-btn">Get in touch</button>
@@ -87,7 +68,7 @@ const buttons = document.querySelectorAll(".button-tag");
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const filterText = button.innerText;
-        createCards(filterText);
+        createCards({ coderCore: filterText });
         setActiveBtn(filterText);
     });
 });
@@ -103,5 +84,19 @@ function setActiveBtn(tag) {
     });
 };
 
+// Escucha del botón de búsqueda
+document.getElementById('search').addEventListener('click', () => {
+    const getSkills = document.getElementById('search-input').value;
+    createCards({ coderSkills: getSkills });
+});
+
+// Escucha de la tecla 'Enter' en el campo de entrada
+document.getElementById('search-input').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        const getSkills = event.target.value;
+        createCards({ coderSkills: getSkills });
+    }
+});
+
 setActiveBtn('All');
-createCards('All');
+createCards({ coderCore: 'All' });
